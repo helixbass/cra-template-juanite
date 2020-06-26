0d13a24a5f93f9095662feb7ce260579522327c2
Julian Rosse <julian@helixbass.net>
2020-06-26 16:08:58 -0400

    adding to template

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {FC} from 'react'
+import {flowMax, addDisplayName} from 'ad-hok'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import {makeStyles} from 'utils/style'
+import colors from 'utils/colors'
 
-export default App;
+const App: FC = flowMax(addDisplayName('App'), () => (
+  <div css={styles.container}>Hello world!</div>
+))
+
+export default App
+
+const styles = makeStyles({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: colors.black,
+    color: colors.white,
+    minHeight: '100vh',
+  },
+})
